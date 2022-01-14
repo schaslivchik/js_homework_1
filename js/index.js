83a10d40c9743b099579dd92b580eecb00eb7c0e
Mikhail <schaslivchikm@gmail.com>
2022-01-14 19:17:57 +0200

    upd

--- js/index.js
+++ js/index.js
@@ -76,12 +76,14 @@
 // (test) ? console.log('Верно') : console.log('Неверно'); 
 
 //3
-// const amount = Number (prompt('Введите суму '));
-// if(amount < 800) {
-//   console.log(amount - (amount * 3 / 100), 'скидка 3%')
-// } else {
-//   console.log(amount - (amount * 5 / 100), 'скидка 5%')
-// }
+const amount = Number (prompt('Введите суму '));
+if(amount >= 500 && amount <= 800) {
+   alert(amount - (amount * 3 / 100) + ' скидка 3%');
+ } else if(amount > 800){
+  alert(amount - (amount * 5 / 100) + ' скидка 5%');
+ } else { 
+     alert('скидки нету');
+ }
 
 
 //loop
@@ -165,45 +167,45 @@
 //     alert('Неверно введено значение')
 // }
 
-const day = +prompt('Введите число от 1 до 31');
-switch(day){
-  case 1:
-  case 2:
-  case 3:
-  case 4:
-  case 5:
-  case 6:
-  case 7:
-  case 8:
-  case 9:
-  case 10:
-    alert('Первая декада');
-    break;
-  case 11:
-  case 12:
-  case 13:
-  case 14:
-  case 15:
-  case 16:
-  case 17:
-  case 18:
-  case 19:
-  case 20:
-    alert('Вторая декада');
-    break;
-  case 21:
-  case 22:
-  case 23:
-  case 24:
-  case 25:
-  case 26:
-  case 27:
-  case 28:
-  case 29:
-  case 30:
-  case 31:  
-    alert('Третья декада');
-    break;
-  default:
-    alert('Неверно введено значение');        
-}
+// const day = +prompt('Введите число от 1 до 31');
+// switch(day){
+//   case 1:
+//   case 2:
+//   case 3:
+//   case 4:
+//   case 5:
+//   case 6:
+//   case 7:
+//   case 8:
+//   case 9:
+//   case 10:
+//     alert('Первая декада');
+//     break;
+//   case 11:
+//   case 12:
+//   case 13:
+//   case 14:
+//   case 15:
+//   case 16:
+//   case 17:
+//   case 18:
+//   case 19:
+//   case 20:
+//     alert('Вторая декада');
+//     break;
+//   case 21:
+//   case 22:
+//   case 23:
+//   case 24:
+//   case 25:
+//   case 26:
+//   case 27:
+//   case 28:
+//   case 29:
+//   case 30:
+//   case 31:  
+//     alert('Третья декада');
+//     break;
+//   default:
+//     alert('Неверно введено значение');        
+// }
